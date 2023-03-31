839c78946206f5c20566a6ae2c5c54b2cc8292ac
Ganesh Chandrasekaran <ganesh.c@samsung.com>
2023-03-31 11:36:08 +0530

    Condition checking test cases added

--- krm-functions/lib/diff/diff_test.go
+++ krm-functions/lib/diff/diff_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
+	kptv1 "github.com/GoogleContainerTools/kpt/pkg/api/kptfile/v1"
 	nadv1 "github.com/k8snetworkplumbingwg/network-attachment-definition-client/pkg/apis/k8s.cni.cncf.io/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -29,7 +30,7 @@ func TestDiffWithSameSpec(t *testing.T) {
 				kind:       "b",
 				name:       "c",
 				spec:       "test1",
-				dummy:      "samsung",
+				dummy:      "sample",
 			},
 		},
 		{
@@ -38,7 +39,7 @@ func TestDiffWithSameSpec(t *testing.T) {
 				kind:       "e",
 				name:       "f",
 				spec:       "test2",
-				dummy:      "samsung",
+				dummy:      "sample",
 			},
 		},
 	}
@@ -77,13 +78,13 @@ func TestDiffWithSameSpec(t *testing.T) {
 	}
 	diffList, _ := inventory.Diff()
 	if len(diffList.CreateObjs) != 0 {
-		t.Errorf("TestGetCondition: -want nothing, +got:\n%v", len(diffList.CreateObjs))
+		t.Errorf("TestDiffWithSameSpec: -want 0, +got:\n%v", len(diffList.CreateObjs))
 	}
 	if len(diffList.DeleteObjs) != 0 {
-		t.Errorf("TestGetCondition: -want nothing, +got:\n%v", len(diffList.DeleteObjs))
+		t.Errorf("TestDiffWithSameSpec: -want 0, +got:\n%v", len(diffList.DeleteObjs))
 	}
 	if len(diffList.UpdateObjs) != 0 {
-		t.Errorf("TestGetCondition: -want nothing, +got:\n%v", len(diffList.UpdateObjs))
+		t.Errorf("TestDiffWithSameSpec: -want 0, +got:\n%v", len(diffList.UpdateObjs))
 	}
 }
 
@@ -105,7 +106,7 @@ func TestDiffWithSpecToDelete(t *testing.T) {
 				apiVersion: "a",
 				kind:       "b",
 				name:       "c",
-				dummy:      "samsung",
+				dummy:      "sample",
 				spec:       "test1",
 			},
 		},
@@ -115,7 +116,7 @@ func TestDiffWithSpecToDelete(t *testing.T) {
 				kind:       "e",
 				name:       "f",
 				spec:       "test2",
-				dummy:      "samsung",
+				dummy:      "sample",
 			},
 		},
 	}
@@ -158,21 +159,19 @@ func TestDiffWithSpecToDelete(t *testing.T) {
 		}
 		byteStream, _ = yaml.Marshal(ipa)
 		kubeObjectMade, _ = fn.ParseKubeObject(byteStream)
-		//inventory.AddNewResource(currentGVKN, kubeObjectMade)
 	}
 	diffList, _ := inventory.Diff()
 	if len(diffList.CreateObjs) != 0 {
-		t.Errorf("TestGetCondition: -want nothing, +got:\n%v", len(diffList.CreateObjs))
+		t.Errorf("TestDiffWithSpecToDelete: -want 0, +got:\n%v", len(diffList.CreateObjs))
 	}
 	if len(diffList.DeleteObjs) != 2 {
-		t.Errorf("TestGetCondition: -want nothing, +got:\n%v", len(diffList.DeleteObjs))
+		t.Errorf("TestDiffWithSpecToDelete: -want 2, +got:\n%v", len(diffList.DeleteObjs))
 	}
 	if len(diffList.UpdateObjs) != 0 {
-		t.Errorf("TestGetCondition: -want nothing, +got:\n%v", len(diffList.UpdateObjs))
+		t.Errorf("TestDiffWithSpecToDelete: -want 0, +got:\n%v", len(diffList.UpdateObjs))
 	}
 }
 
-// TODO: need to update with more conditions
 func TestDiffWithSpecToAdd(t *testing.T) {
 	inventory := New()
 	type object struct {
@@ -191,7 +190,7 @@ func TestDiffWithSpecToAdd(t *testing.T) {
 				apiVersion: "a",
 				kind:       "b",
 				name:       "c",
-				dummy:      "samsung",
+				dummy:      "sample",
 				spec:       "test1",
 			},
 		},
@@ -201,7 +200,7 @@ func TestDiffWithSpecToAdd(t *testing.T) {
 				kind:       "e",
 				name:       "f",
 				spec:       "test2",
-				dummy:      "samsung",
+				dummy:      "sample",
 			},
 		},
 	}
@@ -228,7 +227,6 @@ func TestDiffWithSpecToAdd(t *testing.T) {
 		}
 		byteStream, _ := yaml.Marshal(ipa)
 		kubeObjectMade, _ := fn.ParseKubeObject(byteStream)
-		//inventory.AddExistingResource(currentGVKN, kubeObjectMade)
 
 		ipa = &nadv1.NetworkAttachmentDefinition{
 			TypeMeta: metav1.TypeMeta{
@@ -248,17 +246,16 @@ func TestDiffWithSpecToAdd(t *testing.T) {
 	}
 	diffList, _ := inventory.Diff()
 	if len(diffList.CreateObjs) != 2 {
-		t.Errorf("TestGetCondition: -want nothing, +got:\n%v", len(diffList.CreateObjs))
+		t.Errorf("TestDiffWithSpecToAdd: -want 2, +got:\n%v", len(diffList.CreateObjs))
 	}
 	if len(diffList.DeleteObjs) != 0 {
-		t.Errorf("TestGetCondition: -want nothing, +got:\n%v", len(diffList.DeleteObjs))
+		t.Errorf("TestDiffWithSpecToAdd: -want 0, +got:\n%v", len(diffList.DeleteObjs))
 	}
 	if len(diffList.UpdateObjs) != 0 {
-		t.Errorf("TestGetCondition: -want nothing, +got:\n%v", len(diffList.UpdateObjs))
+		t.Errorf("TestDiffWithSpecToAdd: -want 0, +got:\n%v", len(diffList.UpdateObjs))
 	}
 }
 
-// TODO: need to update with more conditions
 func TestDiffWithSpecToUpdate(t *testing.T) {
 	inventory := New()
 	type object struct {
@@ -278,7 +275,7 @@ func TestDiffWithSpecToUpdate(t *testing.T) {
 				kind:       "b",
 				name:       "c",
 				spec:       "test1",
-				dummy:      "samsung",
+				dummy:      "sample",
 			},
 		},
 		{
@@ -287,7 +284,7 @@ func TestDiffWithSpecToUpdate(t *testing.T) {
 				kind:       "e",
 				name:       "f",
 				spec:       "test2",
-				dummy:      "samsung",
+				dummy:      "sample",
 			},
 		},
 	}
@@ -300,7 +297,7 @@ func TestDiffWithSpecToUpdate(t *testing.T) {
 				kind:       "b",
 				name:       "c",
 				spec:       "test3",
-				dummy:      "samsung",
+				dummy:      "sample",
 			},
 		},
 		{
@@ -309,7 +306,7 @@ func TestDiffWithSpecToUpdate(t *testing.T) {
 				kind:       "e",
 				name:       "f",
 				spec:       "test4",
-				dummy:      "samsung",
+				dummy:      "sample",
 			},
 		},
 	}
@@ -365,12 +362,178 @@ func TestDiffWithSpecToUpdate(t *testing.T) {
 	}
 	diffList, _ := inventory.Diff()
 	if len(diffList.CreateObjs) != 0 {
-		t.Errorf("TestGetCondition: -want nothing, +got:\n%v", len(diffList.CreateObjs))
+		t.Errorf("TestDiffWithSpecToUpdate: -want 0, +got:\n%v", len(diffList.CreateObjs))
 	}
 	if len(diffList.DeleteObjs) != 0 {
-		t.Errorf("TestGetCondition: -want nothing, +got:\n%v", len(diffList.DeleteObjs))
+		t.Errorf("TestDiffWithSpecToUpdate: -want 0, +got:\n%v", len(diffList.DeleteObjs))
 	}
 	if len(diffList.UpdateObjs) != 2 {
-		t.Errorf("TestGetCondition: -want nothing, +got:\n%v", len(diffList.UpdateObjs))
+		t.Errorf("TestDiffWithSpecToUpdate: -want 2, +got:\n%v", len(diffList.UpdateObjs))
+	}
+	if len(diffList.UpdateConditions) != 2 {
+		t.Errorf("TestDiffWithSpecToUpdate: -want 2, +got:\n%v", len(diffList.UpdateConditions))
+	}
+}
+
+func TestDiffWithSpecToAddCondition(t *testing.T) {
+	inventory := New()
+	type object struct {
+		apiVersion string
+		kind       string
+		name       string
+		dummy      string
+		spec       string
+	}
+
+	test1 := []struct {
+		input object
+	}{
+		{
+			input: object{
+				apiVersion: "a",
+				kind:       "b",
+				name:       "c",
+				dummy:      "sample",
+				spec:       "test1",
+			},
+		},
+		{
+			input: object{
+				apiVersion: "d",
+				kind:       "e",
+				name:       "f",
+				spec:       "test2",
+				dummy:      "sample",
+			},
+		},
+	}
+	for _, tt := range test1 {
+		currentGVKN := &corev1.ObjectReference{
+			APIVersion:      tt.input.apiVersion,
+			Kind:            tt.input.kind,
+			Name:            tt.input.name,
+			Namespace:       tt.input.dummy,
+			FieldPath:       tt.input.dummy,
+			ResourceVersion: tt.input.apiVersion,
+		}
+		ipa := &nadv1.NetworkAttachmentDefinition{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: tt.input.apiVersion,
+				Kind:       tt.input.kind,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: tt.input.name,
+			},
+			Spec: nadv1.NetworkAttachmentDefinitionSpec{
+				Config: tt.input.spec,
+			},
+		}
+		byteStream, _ := yaml.Marshal(ipa)
+		kubeObjectMade, _ := fn.ParseKubeObject(byteStream)
+
+		ipa = &nadv1.NetworkAttachmentDefinition{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: tt.input.apiVersion,
+				Kind:       tt.input.kind,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: tt.input.name,
+			},
+			Spec: nadv1.NetworkAttachmentDefinitionSpec{
+				Config: "type2",
+			},
+		}
+		byteStream, _ = yaml.Marshal(ipa)
+		kubeObjectMade, _ = fn.ParseKubeObject(byteStream)
+		inventory.AddNewResource(currentGVKN, kubeObjectMade)
+
+	}
+	diffList, _ := inventory.Diff()
+	if len(diffList.CreateConditions) != 2 {
+		t.Errorf("TestDiffWithSpecToAddCondition: -want 2, +got:\n%v", len(diffList.CreateConditions))
+	}
+}
+
+func TestDiffWithSpecToDeleteCondition(t *testing.T) {
+	inventory := New()
+	type object struct {
+		apiVersion string
+		kind       string
+		name       string
+		dummy      string
+		spec       string
+	}
+
+	test1 := []struct {
+		input object
+	}{
+		{
+			input: object{
+				apiVersion: "a",
+				kind:       "b",
+				name:       "c",
+				dummy:      "sample",
+				spec:       "test1",
+			},
+		},
+		{
+			input: object{
+				apiVersion: "d",
+				kind:       "e",
+				name:       "f",
+				spec:       "test2",
+				dummy:      "sample",
+			},
+		},
+	}
+	for _, tt := range test1 {
+		currentGVKN := &corev1.ObjectReference{
+			APIVersion:      tt.input.apiVersion,
+			Kind:            tt.input.kind,
+			Name:            tt.input.name,
+			Namespace:       tt.input.dummy,
+			FieldPath:       tt.input.dummy,
+			ResourceVersion: tt.input.apiVersion,
+		}
+		ipa := &nadv1.NetworkAttachmentDefinition{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: tt.input.apiVersion,
+				Kind:       tt.input.kind,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: tt.input.name,
+			},
+			Spec: nadv1.NetworkAttachmentDefinitionSpec{
+				Config: tt.input.spec,
+			},
+		}
+		byteStream, _ := yaml.Marshal(ipa)
+		kubeObjectMade, _ := fn.ParseKubeObject(byteStream)
+		inventory.AddExistingCondition(currentGVKN, &kptv1.Condition{
+			Type:    currentGVKN.APIVersion + currentGVKN.Kind + currentGVKN.Name,
+			Status:  kptv1.ConditionFalse,
+			Reason:  tt.input.dummy,
+			Message: tt.input.dummy,
+		})
+		inventory.AddExistingResource(currentGVKN, kubeObjectMade)
+
+		ipa = &nadv1.NetworkAttachmentDefinition{
+			TypeMeta: metav1.TypeMeta{
+				APIVersion: tt.input.apiVersion,
+				Kind:       tt.input.kind,
+			},
+			ObjectMeta: metav1.ObjectMeta{
+				Name: tt.input.name,
+			},
+			Spec: nadv1.NetworkAttachmentDefinitionSpec{
+				Config: "type2",
+			},
+		}
+		byteStream, _ = yaml.Marshal(ipa)
+		kubeObjectMade, _ = fn.ParseKubeObject(byteStream)
+	}
+	diffList, _ := inventory.Diff()
+	if len(diffList.DeleteConditions) != 2 {
+		t.Errorf("TestDiffWithSpecToDeleteCondition: -want 0, +got:\n%v", len(diffList.DeleteConditions))
 	}
 }
