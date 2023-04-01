a1f2b979e4ff1a9940230926f918ae5c0da2fb80
Wim Henderickx <wim.henderickx@gmail.com>
2023-04-01 07:33:41 +0200

    added GetGVKNFromConditionType to convert string to objectReference

--- krm-functions/lib/kptfile/v1/condition.go
+++ krm-functions/lib/kptfile/v1/condition.go
@@ -17,6 +17,7 @@ limitations under the License.
 package v1
 
 import (
+	"fmt"
 	"strings"
 
 	corev1 "k8s.io/api/core/v1"
@@ -45,4 +46,23 @@ func GetConditionType(o *corev1.ObjectReference) string {
 		sb.WriteString(o.Name)
 	}
 	return sb.String()
+}
+
+func GetGVKNFromConditionType(ct string) (o *corev1.ObjectReference) {
+	split := strings.Split(ct, "/")
+	group := ""
+	vkn := ct
+	if len(split) > 1 {
+		group = split[0]
+		vkn = split[1]
+	}
+	newsplit := strings.Split(vkn, ".")
+	if len(newsplit) == 3 {
+		return &corev1.ObjectReference{
+			APIVersion: fmt.Sprintf("%s/%s", group, newsplit[0]),
+			Kind:       newsplit[1],
+			Name:       newsplit[2],
+		}
+	}
+	return &corev1.ObjectReference{}
 }
