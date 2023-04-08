7624b8df70bc2f4b2007bc92563d19d91d731915
Ganesh Chandrasekaran <ganesh.c@samsung.com>
2023-04-08 09:40:34 +0530

    removing organisation specific copyright

--- krm-functions/lib/nad/v1/nad_test.go
+++ krm-functions/lib/nad/v1/nad_test.go
@@ -1,6 +1,5 @@
 /*
 Copyright 2023 Nephio.
-Copyright 2023 Samsung.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
