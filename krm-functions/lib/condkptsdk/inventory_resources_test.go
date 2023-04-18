f07bf9aaa2c3969ecb78165b946795453f1741e4
Wim Henderickx <wim.henderickx@gmail.com>
2023-04-18 21:53:52 +0200

    added resource tests

--- krm-functions/lib/condkptsdk/inventory_resources_test.go
+++ krm-functions/lib/condkptsdk/inventory_resources_test.go
@@ -16,3 +16,57 @@
 
 package condkptsdk
 
+import (
+	"testing"
+
+	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
+	"github.com/stretchr/testify/assert"
+	corev1 "k8s.io/api/core/v1"
+)
+
+func TestSet(t *testing.T) {
+	cases := map[string]struct {
+		kc          *gvkKindCtx
+		refs        []corev1.ObjectReference
+		x           any
+		new         newResource
+		errExpected bool
+	}{
+		"ForNormal": {
+			kc: &gvkKindCtx{gvkKind: forGVKKind},
+			refs: []corev1.ObjectReference{
+				{APIVersion: "a", Kind: "a", Name: "a"},
+			},
+			x:           fn.NewEmptyKubeObject(),
+			errExpected: false,
+		},
+		"ForErrorRefs": {
+			kc: &gvkKindCtx{gvkKind: forGVKKind},
+			refs: []corev1.ObjectReference{
+				{APIVersion: "a", Kind: "a", Name: "a"},
+				{APIVersion: "a", Kind: "a", Name: "a"},
+			},
+			x:           fn.NewEmptyKubeObject(),
+			errExpected: true,
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			i, err := newInventory(&Config{
+				For:                corev1.ObjectReference{APIVersion: "a", Kind: "a"},
+				GenerateResourceFn: GenerateResourceFnNop,
+			})
+			if err != nil {
+				assert.NoError(t, err)
+			}
+
+			err = i.set(tc.kc, tc.refs, tc.x, tc.new)
+			if tc.errExpected {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+		})
+	}
+}
