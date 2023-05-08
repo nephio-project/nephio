0c5fc16776cffb2a327b1a17d6fee497701061cd
Wim Henderickx <wim.henderickx@gmail.com>
2023-05-08 15:14:32 -0400

    added vlan-fn

---
+++ krm-functions/vlan-fn/fn/function.go
@@ -0,0 +1,81 @@
+/*
+ Copyright 2023 The Nephio Authors.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+*/
+
+package fn
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
+	"github.com/nephio-project/nephio/krm-functions/lib/condkptsdk"
+	"github.com/nephio-project/nephio/krm-functions/lib/kubeobject"
+	vlanv1alpha1 "github.com/nokia/k8s-ipam/apis/alloc/vlan/v1alpha1"
+	"github.com/nokia/k8s-ipam/pkg/proxy/clientproxy"
+	corev1 "k8s.io/api/core/v1"
+)
+
+type FnR struct {
+	ClientProxy clientproxy.Proxy[*vlanv1alpha1.VLANDatabase, *vlanv1alpha1.VLANAllocation]
+}
+
+func (r *FnR) Run(rl *fn.ResourceList) (bool, error) {
+	sdk, err := condkptsdk.New(
+		rl,
+		&condkptsdk.Config{
+			For: corev1.ObjectReference{
+				APIVersion: vlanv1alpha1.GroupVersion.Identifier(),
+				Kind:       vlanv1alpha1.VLANAllocationKind,
+			},
+			PopulateOwnResourcesFn: nil,
+			GenerateResourceFn:     r.updateVLANAllocationResource,
+		},
+	)
+	if err != nil {
+		rl.Results.ErrorE(err)
+		return false, nil
+	}
+	return sdk.Run()
+}
+
+// updateIPAllocationResource provides a VLAN allocation for a given VLANAllocation KRM resource
+// in the package by calling the vlan backend
+func (r *FnR) updateVLANAllocationResource(forObj *fn.KubeObject, objs fn.KubeObjects) (*fn.KubeObject, error) {
+	if forObj == nil {
+		return nil, fmt.Errorf("expected a for object but got nil")
+	}
+	fn.Logf("vlanalloc: %v\n", forObj)
+	allocKOE, err := kubeobject.NewFromKubeObject[vlanv1alpha1.VLANAllocation](forObj)
+	if err != nil {
+		return nil, err
+	}
+	alloc, err := allocKOE.GetGoStruct()
+	if err != nil {
+		return nil, err
+	}
+	resp, err := r.ClientProxy.Allocate(context.Background(), alloc, nil)
+	if err != nil {
+		return nil, err
+	}
+	alloc.Status = resp.Status
+
+	if alloc.Status.VLANID != nil {
+		fn.Logf("alloc resp vlan: %v\n", *resp.Status.VLANID)
+	}
+	// set the status
+	err = allocKOE.SetStatus(resp)
+	return &allocKOE.KubeObject, err
+}
