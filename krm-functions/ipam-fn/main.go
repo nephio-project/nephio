92d80526d9808c77a4b2f394e8e671b5ed7c5900
Wim Henderickx <wim.henderickx@gmail.com>
2023-05-11 05:01:33 -0400

    docker update for interface-fn/ipam-fn/vlan-fn (#186)
    updated docker for interface-fn/ipam-fn/vlan-fn
    missing main.go

--- krm-functions/ipam-fn/main.go
+++ krm-functions/ipam-fn/main.go
@@ -20,12 +20,16 @@ import (
 	"os"
 
 	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
-	fnr "github.com/nephio-project/nephio/krm-functions/interface-fn/fn"
+	fnr "github.com/nephio-project/nephio/krm-functions/ipam-fn/fn"
+	"github.com/nokia/k8s-ipam/pkg/proxy/clientproxy/ipam"
 )
 
 func main() {
+	r := &fnr.FnR{
+		ClientProxy: ipam.NewMock(),
+	}
 
-	if err := fn.AsMain(fn.ResourceListProcessorFunc(fnr.Run)); err != nil {
+	if err := fn.AsMain(fn.ResourceListProcessorFunc(r.Run)); err != nil {
 		os.Exit(1)
 	}
 }
