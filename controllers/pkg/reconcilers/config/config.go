45e13a82427f4055b9c284dce80d6c2001be7c7e
ssharmasandeep <ssharma@aarnanetworks.com>
2023-05-18 23:23:51 +0530

    Removed the gitea-clinet import until, let it come to nephio repo and then we will use it
    Signed-off-by: ssharmasandeep <ssharma@aarnanetworks.com>

--- controllers/pkg/reconcilers/config/config.go
+++ controllers/pkg/reconcilers/config/config.go
@@ -19,15 +19,14 @@ package ctrlrconfig
 import (
 	"time"
 
-	"github.com/henderiw-nephio/nephio-controllers/pkg/giteaclient"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 )
 
 type ControllerConfig struct {
 	PorchClient client.Client
-	GiteaClient giteaclient.GiteaClient
-	Poll        time.Duration
-	Copts       controller.Options
-	Address     string // backend server address
+	//GiteaClient giteaclient.GiteaClient
+	Poll    time.Duration
+	Copts   controller.Options
+	Address string // backend server address
 }
