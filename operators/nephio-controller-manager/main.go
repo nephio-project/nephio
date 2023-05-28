3eab5b9ca05ff9edfb746c567bdbb7778af03be6
Wim Henderickx <wim.henderickx@gmail.com>
2023-05-29 00:23:04 +0200

    added bootstrappers (#232)
    added the package and secret bootstrapped to nephio operator

--- operators/nephio-controller-manager/main.go
+++ operators/nephio-controller-manager/main.go
@@ -42,6 +42,8 @@ import (
 	//+kubebuilder:scaffold:imports
 
 	// Import our reconcilers
+	_ "github.com/nephio-project/nephio/controllers/pkg/reconcilers/bootstrap-packages"
+	_ "github.com/nephio-project/nephio/controllers/pkg/reconcilers/bootstrap-secret"
 	_ "github.com/nephio-project/nephio/controllers/pkg/reconcilers/ipam-specializer"
 	_ "github.com/nephio-project/nephio/controllers/pkg/reconcilers/repository"
 	_ "github.com/nephio-project/nephio/controllers/pkg/reconcilers/token"
