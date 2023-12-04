e6afeb856842f3af2809c4690675744d698ef13d
Wim Henderickx <wim.henderickx@gmail.com>
2023-12-04 19:58:54 +0100

    added logic for multiple clusters

--- controllers/pkg/reconcilers/bootstrap-secret/README.md
+++ controllers/pkg/reconcilers/bootstrap-secret/README.md
@@ -6,9 +6,13 @@ The bootstrap controller is a k8s controller which goal is to bootstrap secrets 
 
 The controller acts on a secret. It first figures out if the secret is to be installed on the remote cluster, by checking if:
 - annotation key `nephio.org/app` is equal to `tobeinstalledonremotecluster`
-- annotation key `nephio.org/cluster-name` is not an empty string or `mgmt`
+- annotation key `nephio.org/cluster-name` is not an empty string or `mgmt`; a cluster-name can contain multiple clusters in a comma seperated list w/o spaces e.g. `nephio.org/cluster-name = cluster01,cluster02`
 
-If the controller knows the secret is to be installed on the remote cluster, it finds the credentials of the remote cluster and the type of cluster based on the signatures of the secret (right now only cluster api is implemented, but the code is able to handle other implementations).
+Per cluster specified in the cluster-name we follow the below process. Bothe need to succeed, if not a reconciliation is retriggered.
+
+per cluser logic:
+
+- If the controller knows the secret is to be installed on the remote cluster, it finds the credentials of the remote cluster and the type of cluster based on the signatures of the secret (right now only cluster api is implemented, but the code is able to handle other implementations).
 Once the remote credentials are found and the cluster is deemed ready, the secret gets installed on the remote cluster after validating if the corresponding namespace exists. The secret get installed on the remote cluster if the corresponding namespace exists
 
 If any of the validation fail the controller will retry installing the secret.
--- controllers/pkg/reconcilers/bootstrap-secret/reconciler.go
+++ controllers/pkg/reconcilers/bootstrap-secret/reconciler.go
@@ -97,80 +97,98 @@ func (r *reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Resu
 		cr.GetAnnotations()[clusterNameKey] != "" &&
 		cr.GetAnnotations()[clusterNameKey] != "mgmt" {
 		log.Info("reconcile secret")
+		// a clusterName can be modelled as multiple clusters to allow
+		// the secret to be deployed on multiple clusters
+		// syntax: nephio.org/cluster-name = cluster01,cluster02,cluster03
 		clusterName := cr.GetAnnotations()[clusterNameKey]
+		clusterNames := strings.Split(clusterName, ",")
 
-		secrets := &corev1.SecretList{}
-		if err := r.List(ctx, secrets); err != nil {
-			msg := "cannot list secrets"
-			log.Error(err, msg)
-			return ctrl.Result{}, errors.Wrap(err, msg)
-		}
-		found := false
-		for _, secret := range secrets.Items {
-			if strings.Contains(secret.GetName(), clusterName) {
-				secret := secret // required to prevent gosec warning: G601 (CWE-118): Implicit memory aliasing in for loop
-				clusterClient, ok := cluster.Cluster{Client: r.Client}.GetClusterClient(&secret)
-				if ok {
-					found = true
-					clusterClient, ready, err := clusterClient.GetClusterClient(ctx)
-					if err != nil {
-						msg := "cannot get clusterClient"
-						log.Error(err, msg)
-						return ctrl.Result{RequeueAfter: 30 * time.Second}, errors.Wrap(err, msg)
-					}
-					if !ready {
-						log.Info("cluster not ready")
-						return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
-					}
-
-					// remoteNamespace holds the namespace of the remote cluster
-					// on which this secret is to be installed.
-					// e.g. configsync requires it to be installed in configsync-management
-					// but the mgmt cluster already has configsync-management in use, so
-					// we need the ability to change the namespace of the remote cluster
-					// for this secret
-					// the controller uses the namespace of the cr by default and if the
-					// remoteNamespace annotation `"nephio.org/remote-namespace"` is set
-					// it will use the value of the  annotation as the remote namespace
-					remoteNamespace := cr.Name
-					if rns, ok := cr.GetAnnotations()[remoteNamespaceKey]; ok {
-						remoteNamespace = rns
-					}
-					// check if the remote namespace exists, if not retry
-					ns := &corev1.Namespace{}
-					if err = clusterClient.Get(ctx, types.NamespacedName{Name: remoteNamespace}, ns); err != nil {
-						if resource.IgnoreNotFound(err) != nil {
-							msg := fmt.Sprintf("cannot get namespace: %s", remoteNamespace)
+		
+		// for each cluster specified in the annotation, we need to find the
+		// corresponding cluster credentials to access th remote cluster
+		// we walk through the secrets and check if the cluster api client accepts them
+		// if so we check is the assigned namespace is available and if so we apply the
+		// secret to the remote cluster.
+		for _, clusterName := range clusterNames {
+			secrets := &corev1.SecretList{}
+			if err := r.List(ctx, secrets); err != nil {
+				msg := "cannot list secrets"
+				log.Error(err, msg)
+				return ctrl.Result{}, errors.Wrap(err, msg)
+			}
+			found := false
+			for _, secret := range secrets.Items {
+				if strings.Contains(secret.GetName(), clusterName) {
+					secret := secret // required to prevent gosec warning: G601 (CWE-118): Implicit memory aliasing in for loop
+					clusterClient, ok := cluster.Cluster{Client: r.Client}.GetClusterClient(&secret)
+					if ok {
+						found = true
+						clusterClient, ready, err := clusterClient.GetClusterClient(ctx)
+						if err != nil {
+							msg := "cannot get clusterClient"
 							log.Error(err, msg)
 							return ctrl.Result{RequeueAfter: 30 * time.Second}, errors.Wrap(err, msg)
 						}
-						msg := fmt.Sprintf("namespace: %s, does not exist, retry...", remoteNamespace)
-						log.Info(msg)
-						return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
-					}
+						if !ready {
+							log.Info("cluster not ready")
+							return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
+						}
 
-					newcr := cr.DeepCopy()
-					// since the original annotations are set by configsync we need to reset them
-					// so apply 2 annotations to the secret: app = bootstrap +  cluster-name = clusterName
-					newcr.SetAnnotations(map[string]string{
-						nephioAppKey:   bootstrapApp,
-						clusterNameKey: clusterName,
-					})
-					newcr.ResourceVersion = ""
-					newcr.UID = ""
-					log.Info("secret info", "secret", newcr.Annotations)
-					if err := clusterClient.Apply(ctx, newcr); err != nil {
-						msg := fmt.Sprintf("cannot apply secret to cluster %s", clusterName)
-						log.Error(err, msg)
-						return ctrl.Result{}, errors.Wrap(err, msg)
+						// remoteNamespace holds the namespace of the remote cluster
+						// on which this secret is to be installed.
+						// e.g. configsync requires it to be installed in configsync-management
+						// but the mgmt cluster already has configsync-management in use, so
+						// we need the ability to change the namespace of the remote cluster
+						// for this secret
+						// the controller uses the namespace of the cr by default and if the
+						// remoteNamespace annotation `"nephio.org/remote-namespace"` is set
+						// it will use the value of the  annotation as the remote namespace
+						remoteNamespace := cr.Name
+						if rns, ok := cr.GetAnnotations()[remoteNamespaceKey]; ok {
+							remoteNamespace = rns
+						}
+						// check if the remote namespace exists, if not retry
+						ns := &corev1.Namespace{}
+						if err = clusterClient.Get(ctx, types.NamespacedName{Name: remoteNamespace}, ns); err != nil {
+							if resource.IgnoreNotFound(err) != nil {
+								msg := fmt.Sprintf("cannot get namespace: %s", remoteNamespace)
+								log.Error(err, msg)
+								return ctrl.Result{RequeueAfter: 30 * time.Second}, errors.Wrap(err, msg)
+							}
+							msg := fmt.Sprintf("namespace: %s, does not exist, retry...", remoteNamespace)
+							log.Info(msg)
+							return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
+						}
+
+						newcr := cr.DeepCopy()
+						// since the original annotations are set by configsync we need to reset them
+						// so apply 2 annotations to the secret: app = bootstrap +  cluster-name = clusterName
+						newcr.SetAnnotations(map[string]string{
+							nephioAppKey:   bootstrapApp,
+							clusterNameKey: clusterName,
+						})
+						newcr.ResourceVersion = ""
+						newcr.UID = ""
+						log.Info("secret info", "secret", newcr.Annotations)
+						if err := clusterClient.Apply(ctx, newcr); err != nil {
+							msg := fmt.Sprintf("cannot apply secret to cluster %s", clusterName)
+							log.Error(err, msg)
+							return ctrl.Result{}, errors.Wrap(err, msg)
+						}
 					}
 				}
+				if found {
+					// speeds up the loop
+					break
+				}
 			}
-		}
-		if !found {
-			// the cluster client was not found, we retry
-			log.Info("cluster client not found, retry...")
-			return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
+			// we need to find a cluster for each clusterName, so
+			// even in case of multiple 
+			if !found {
+				// the cluster client was not found, we retry
+				log.Info("cluster client not found, retry...")
+				return ctrl.Result{RequeueAfter: 10 * time.Second}, nil
+			}
 		}
 	}
 	return ctrl.Result{}, nil
