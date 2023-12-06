2e70ac48d384c91eda6b5554648999d1323973d7
Vishwanath Jayaraman <vjayaram@redhat.com>
2023-12-06 15:04:11 -0600

    refactor makefiles to ensure mockery is invoked in pkg directories
    Co-authored-by: Daniel Kostecki <84861140+dkosteck@users.noreply.github.com>

--- default-mockery.mk
+++ default-mockery.mk
@@ -18,12 +18,3 @@ GIT_ROOT_DIR ?= $(dir $(lastword $(MAKEFILE_LIST)))
 OS_ARCH ?= $(shell uname -m)
 OS ?= $(shell uname)
 include $(GIT_ROOT_DIR)/detect-container-runtime.mk
-
-
-.PHONY: install-mockery
-install-mockery: ## install mockery
-ifeq ($(CONTAINER_RUNNABLE), 0)
-		$(CONTAINER_RUNTIME) pull docker.io/vektra/mockery:v${MOCKERY_VERSION}
-else
-		wget -qO- https://github.com/vektra/mockery/releases/download/v${MOCKERY_VERSION}/mockery_${MOCKERY_VERSION}_${OS}_${OS_ARCH}.tar.gz | sudo tar -xvzf - -C /usr/local/bin
-endif
