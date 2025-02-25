3431764784e5f999d67abdab57de6f27ba392a02
Gaurav Kumar <gauravkr19@gmail.com>
2025-02-25 09:23:37 +0530

    updated to Copyright 2025 from Copyright 2023

--- krm-functions/nad-fn/main.go
+++ krm-functions/nad-fn/main.go
@@ -1,5 +1,5 @@
 /*
- Copyright 2023 The Nephio Authors.
+ Copyright 2025 The Nephio Authors.
 
  Licensed under the Apache License, Version 2.0 (the "License");
  You may not use this file except in compliance with the License.
