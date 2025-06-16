db8fdf40ec192643e9e5e39a98c82537762a63f4
Daniel Kostecki <dkosteck@redhat.com>
2025-06-16 17:17:52 -0400

    ArgoCD KPT Config Management Plugins:
    Sources for kpt-repo and kpt-render
    Build files for each CMP
    Patch for argocd-repo-server
    Prow job to create images

---
+++ gitops-tools/kpt-argocd-cmp/README.md
@@ -0,0 +1,14 @@
+# kpt-argocd-cmp
+Nephio heavily relies on kpt to package, render, mutate, validate and generate Kubernetes objects. ArgoCD doesn't currently have a built-in plugin to handle installation of manifests, and, as such, this repo introduces a plugin specifically built to render the kpt package pipeline properly. It consists of two [Conifg Management Plugins](https://argo-cd.readthedocs.io/en/stable/operator-manual/config-management-plugins/) (CMPs) for ArgoCD that handle the creation of package applications and local configs.
+
+## kpt-repo
+This plugin creates an "app-of-apps" style ArgoCD Application that takes a source repository and looks for Kptfiles to create ArgoCD applications for the cooresponding packages.
+
+## kpt-render
+The applications created by `kpt-repo` will target a second plugin, `kpt-render`, that filters out KRM files with `config.kubernetes.io/local-config: "true"` or that are Kustomize files. This deals with a primary limitation of ArgoCD, where for plain yaml packages it will attempt to install `local-config` manifests into the destination cluster.  
+
+## patch.sh
+This file applies the plugins to the `argocd-repo-server` pod, using the images created via the corresponding Dockerfiles and pushed to a registery. Once patched, `argo-repo-server` will create containers for each plugin based on the images provided, and start the plugins. 
+
+## Usage
+In order for one to use the plugin, our prefered method is to target `kpt-repo` as the plugin for an ArgoCD Application. This mapping works in our use case as this "app-of-apps" Application represents a repository source and cluster destination. There are other methods to target CMPs (such as discovery rules) that are outside of the scope of this work.
---
+++ gitops-tools/kpt-argocd-cmp/kpt-repo/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"github.com/GoogleContainerTools/kpt-functions-sdk/go/fn"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func outputArgoApplication(kptFile *fn.KubeObject, path string) error {
+	destinationName := os.Getenv("ARGOCD_ENV_DESTINATION_NAME") 
+	fmt.Fprintf(os.Stderr, "destination:\n")
+       	fmt.Fprintf(os.Stderr, destinationName)
+	projectName := os.Getenv("ARGOCD_APP_PROJECT_NAME") 
+	fmt.Fprintf(os.Stderr, "projectName:\n")
+        fmt.Fprintf(os.Stderr, projectName)
+	if destinationName == "" || projectName == "" {
+		return nil
+	}
+	dirPath := os.Getenv("ARGOCD_APP_SOURCE_PATH") + "/" + filepath.Dir(path)
+	fmt.Fprintf(os.Stderr, "dirPath:\n")
+        fmt.Fprintf(os.Stderr, dirPath)
+	name := kptFile.GetName()
+	
+	repo_url := kptFile.GetString("repo")
+
+	ko, err := fn.ParseKubeObject([]byte(`
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: app
+  namespace: argocd
+spec:
+  destination:
+    namespace: default
+    name: __NAME__
+  project: __PROJECT__
+  source:
+    path: __PATH__
+    plugin:
+      name: kpt-render
+      env:
+    repoURL: __REPO_URL__
+    targetRevision: main
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true`))
+	if err != nil {
+		return err
+	}
+	if repo_url == "" {
+		ko.SetNestedField(os.Getenv("ARGOCD_APP_SOURCE_REPO_URL"), "spec", "source", "repoURL")
+	} else {
+		ko.SetNestedField(repo_url, "spec", "source", "repoURL")
+	}
+	ko.SetNestedField(dirPath, "spec", "source", "path")
+	ko.SetNestedField(destinationName, "spec", "destination", "name")
+	ko.SetNestedField(projectName, "spec", "project")
+	ko.SetName(name)
+	fmt.Print(ko.String())
+	fmt.Println("---")
+	return nil
+}
+
+func main() {
+	err := filepath.Walk(".",
+		func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			if strings.HasSuffix(path, "Kptfile") {
+				bytes, err := os.ReadFile(path)
+				if err != nil {
+					return err
+				}
+				ko, err := fn.ParseKubeObject(bytes)
+				if err != nil {
+					return err
+				}
+				fmt.Fprintf(os.Stderr, "path:\n")
+        			fmt.Fprintf(os.Stderr, path)
+				err = outputArgoApplication(ko, path)
+				if err != nil {
+					return err
+				}
+			}
+
+			return nil
+		})
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
